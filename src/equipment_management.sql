89f14d7870b934840f2408990b5c48a72a192ffd
GraceLee-ada <grace.lee@ada.ac.uk>
2025-01-31 15:26:44 +0000

    Days figured out

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -9,9 +9,15 @@ PRAGMA foreign_keys = ON;
 
 -- 1. Find equipment due for maintenance
 -- TODO: Write a query to find equipment due for maintenance
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE strftime('%j', next_maintenance_date) < strftime('%j', date('now', '+30 day')) = 1;
 
 -- 2. Count equipment types in stock
 -- TODO: Write a query to count equipment types in stock
+SELECT type as equipment_type, COUNT(type) as count
+FROM equipment
+GROUP BY type
 
 -- 3. Calculate average age of equipment by type (in days)
 -- TODO: Write a query to calculate average age of equipment by type (in days)
