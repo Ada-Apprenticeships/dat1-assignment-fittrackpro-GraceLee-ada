3f51059c5c9e9a1388e0e6bf20e9bd57ef7be9e0
GraceLee-ada <grace.lee@ada.ac.uk>
2025-02-05 13:49:31 +0000

    2 more queries complete in attendance_tracking.sql

--- src/attendance_tracking.sql
+++ src/attendance_tracking.sql
@@ -9,12 +9,21 @@ PRAGMA foreign_keys = ON;
 
 -- 1. Record a member's gym visit
 -- TODO: Write a query to record a member's gym visit
+-- INSERT INTO attendance(check_in_time, member_id, location_id) VALUES
+-- (datetime('now'), 7, 1);
 
 -- 2. Retrieve a member's attendance history
 -- TODO: Write a query to retrieve a member's attendance history
+SELECT date(check_in_time) AS visit_date, check_in_time, check_out_time
+FROM attendance
+WHERE member_id = 5;
 
 -- 3. Find the busiest day of the week based on gym visits
 -- TODO: Write a query to find the busiest day of the week based on gym visits
+SELECT strftime('%u',check_in_time) AS day_of_week, COUNT(strftime('%u',check_in_time)) AS visit_count
+FROM attendance
+GROUP BY strftime('%u',check_in_time)
+ORDER BY COUNT(strftime('%u',check_in_time)) DESC LIMIT 1;
 
 -- 4. Calculate the average daily attendance for each location
 -- TODO: Write a query to calculate the average daily attendance for each location
