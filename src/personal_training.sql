54a28eb605647f86d283bb0d34d5ca220af31ec7
GraceLee-ada <grace.lee@ada.ac.uk>
2025-02-14 11:31:01 +0000

    Changed personal_training.sql

--- src/personal_training.sql
+++ src/personal_training.sql
@@ -10,8 +10,7 @@ PRAGMA foreign_keys = ON;
 -- 1. List all personal training sessions for a specific trainer
 -- TODO: Write a query to list all personal training sessions for a specific trainer
 
---CHANGE
 SELECT pts.session_id, m.first_name||' '||m.last_name AS member_name, pts.session_date, pts.start_time, pts.end_time
 FROM personal_training_sessions pts
 INNER JOIN members m ON pts.member_id = m.member_id
-WHERE m.first_name||' '||m.last_name = 'Ivy Irwin';
+INNER JOIN staff s ON pts.staff_id = s.staff_id AND s.first_name||' '||s.last_name = 'Ivy Irwin' ;
